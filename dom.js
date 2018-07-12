4d33d5df247921bf2a5003d4e0a211cd8233a3dc
Joe Friel <joseph.s.friel@gmail.com>
2018-07-12 12:27:48 +0100

    Added working marktodo button through DOM

--- dom.js
+++ dom.js
@@ -7,9 +7,9 @@
   var addTodoForm = document.getElementById('add-todo');
 
   var state = [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
+    { id: -3, description: 'first todo', done: false, priority: 'now' },
+    { id: -2, description: 'second todo', done: false, priority: 'today' },
+    { id: -1, description: 'third todo', done: false, priority: 'none' },
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
@@ -28,6 +28,13 @@
     todoNode.appendChild(deleteButtonNode);
 
     // add markTodo button
+    var markButtonNode = document.createElement('button');
+    markButtonNode.addEventListener('click', function(event) {
+      var newState = todoFunctions.markTodo(state, todo.id);
+      update(newState);
+      console.log(state);
+    });
+    todoNode.appendChild(markButtonNode);
 
     // add classes for css
 
